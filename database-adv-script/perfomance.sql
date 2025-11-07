afe67a5437f9d0ebd26a572e3655d4425ed50ecf
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-07 23:21:07 +0300

    fix: add EXPLAIN and WHERE AND clause to perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,9 @@
 -- Initial complex query: Retrieve bookings with user, property, and payment
+EXPLAIN
 SELECT b.*, u.*, p.*, pay.*
 FROM Booking b
          INNER JOIN User u ON b.user_id = u.id
          INNER JOIN Property p ON b.property_id = p.id
-         LEFT JOIN Payment pay ON b.payment_id = pay.id;
+         LEFT JOIN Payment pay ON b.payment_id = pay.id
+WHERE b.status = 'confirmed'
+AND p.is_available = TRUE;
