4994f55c43d32d84b65108bef58c79a2dbc00608
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-07 23:15:52 +0300

    fix: add EXPLAIN ANALYZE commands to database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,7 +1,40 @@
 -- Indexes for high-usage columns
+
+-- Performance analysis before creating indexes
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 1;
+
+EXPLAIN ANALYZE
+SELECT * FROM Review WHERE property_id = 1;
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE property_id = 1;
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE start_date >= '2024-01-01';
+
+EXPLAIN ANALYZE
+SELECT * FROM Review WHERE rating >= 4;
+
 CREATE INDEX idx_user_id ON Booking(user_id);
 CREATE INDEX idx_property_id ON Review(property_id);
 CREATE INDEX idx_property_id_booking ON Booking(property_id);
 CREATE INDEX idx_start_date_booking ON Booking(start_date);
 CREATE INDEX idx_review_rating ON Review(rating);
+
+-- Performance analysis after creating indexes
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 1;
+
+EXPLAIN ANALYZE
+SELECT * FROM Review WHERE property_id = 1;
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE property_id = 1;
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE start_date >= '2024-01-01';
+
+EXPLAIN ANALYZE
+SELECT * FROM Review WHERE rating >= 4;
 
